ac5e56187c4c730711ca3f8039ba1d3c5c366aec
Anthony Loukinas <anthony.loukinas@gmail.com>
2018-12-18 01:10:05 -0500

    Removing build from repository

--- build/usr/share/ansible/module_utils/xenserver_common.py
+++
@@ -1,9 +0,0 @@
-'''xenserver_common
-'''
-class XeBase(object):
-    """
-    This is a xe command generic class
-    """
-    def __init__(self, module):
-        self.module = module
-        self.cmd = ['xe']
